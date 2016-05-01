da42c1e7304b0b9aca3d4d33591917fc1cacce25
Sebastian Fairchild <sfairchild@hubspot.com>
2016-05-01 09:15:56 -0400

    update

--- routes/api/hubspot.js
+++ routes/api/hubspot.js
@@ -66,7 +66,7 @@ router.get('/', function(req, res){
 
 router.post('/', function(req, res){
   res.status(200);
-  console.log(req.headers);
+  console.log(atob(req.headers.authorization.split(' ')[1]));
   res.end();
 });
 
