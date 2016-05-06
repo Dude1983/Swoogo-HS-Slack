b5c003bda6e0285c264dae28c0f65b8b434d5353
Sebastian Fairchild <sfairchild@hubspot.com>
2016-05-06 11:39:48 -0400

    formatNewLeadPostBody

--- routes/api/hubspot.js
+++ routes/api/hubspot.js
@@ -90,7 +90,7 @@ router.post('/lead', function(req, res){
       res.status(401).send("Unauthorized").end();
     } else {
       org = d[0].organization;
-      hsUtils.formatNewLeadPostBody(d.selected_properties, response);
+      hsUtils.formatNewLeadPostBody(d[0].selected_properties, response);
       res.status(200).end();
     }
   });
