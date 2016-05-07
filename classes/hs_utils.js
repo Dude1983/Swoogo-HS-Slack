7c64b25e9136783a0c14182b968e65036d5718c3
Sebastian Fairchild <sfairchild@hubspot.com>
2016-05-07 09:52:47 -0400

    update

--- classes/hs_utils.js
+++ classes/hs_utils.js
@@ -168,7 +168,7 @@ function formatNewLeadPostBody (id, metaData, post){
       }
   });
 
-  message.text += `<a href="${}">see record</a>`;
+  message.text += `<a href="${post['profile-url']}">see record</a>`;
 
   slackUtils.getToken(id, slackUtils.postMessage, message);
 }
